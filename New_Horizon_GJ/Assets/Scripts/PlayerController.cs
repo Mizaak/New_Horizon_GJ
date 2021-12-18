aee64968479f641043e202474c4c0aefab0fc106
EHPad <leonardo.pedrina@gmail.com>
2021-12-18 18:44:57 +0100

    game level block out and first player, prefabs for player movement and rooms

--- New_Horizon_GJ/Assets/Scripts/PlayerController.cs
+++ New_Horizon_GJ/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    private float playerSpeed = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +22,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        playerVelocity.y = 0;
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
@@ -42,13 +37,6 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Walking",false);
         }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
